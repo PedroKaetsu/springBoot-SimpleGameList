d231f3abeea698af2d1756ae6cb5856bec6636c1
Pedro Kaetsu <phksardeto@gmail.com>
2024-11-17 17:33:39 -0300

    Add new game DTO, getByID controller endpoint and findByID method in service

--- src/main/java/com/kaetsu/gamelist/services/GameService.java
+++ src/main/java/com/kaetsu/gamelist/services/GameService.java
@@ -2,12 +2,14 @@ package com.kaetsu.gamelist.services;
 
 import java.util.List;
 
+import com.kaetsu.gamelist.dto.GameDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.kaetsu.gamelist.entities.Game;
 import com.kaetsu.gamelist.dto.GameMinDTO;
 import com.kaetsu.gamelist.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GameService {
@@ -15,6 +17,17 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    // This annotation makes the method be executed in a transactional context, respecting the ACID rules
+    // ACID - Atomic, Consistent, Isolated, Durable
+    // readOnly indicates that the method only reads data from the database, so it can be optimized
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        // TODO: Handle the case when the game is not found
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         // Stream method allows to do operations on a collection of objects
